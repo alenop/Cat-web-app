ff8abbb5a21d7e48a78cfd50de9fefb11995de02
Sorli Pierre <bert.dud2011@gmail.com>
2022-02-24 23:38:40 +0100

    add basic Spring Java for backend and basic vueJS for the frontend

---
+++ CatWebAppBack/src/main/java/com/atelier/CatWebApp/CatWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.atelier.CatWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatWebAppApplication.class, args);
+	}
+
+}
---
+++ CatWebAppBack/src/test/java/com/atelier/CatWebApp/CatWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atelier.CatWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

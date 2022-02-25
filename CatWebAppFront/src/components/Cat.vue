b476dfefe683ead27df204ec531d918f426e965b
Sorli Pierre <bert.dud2011@gmail.com>
2022-02-25 16:40:09 +0100

    add CatComponent with CatComponentcss and begin of page Vote

---
+++ CatWebAppFront/src/components/Cat.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  CatName: {
+    type: String,
+    required: true,
+  },
+  Tmp:{
+    type: String,
+    required: true,
+  }
+});
+</script>
+<template>
+  <div id="Content">
+    <h1 class="titre">{{CatName}}</h1>
+    <div class="col-size">
+    <div class="vert-center">
+    <p @click="send()">{{Tmp}}</p>
+    </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    methods:{
+        send(){
+            console.log(this.Tmp)
+        }
+    }
+}
+/*<img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
+    */
+</script>
+<style scoped>
+@import '../assets/style/CatComponent.css' 
+</style>

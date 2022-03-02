be2253586edb75a5cc78da249d668654f388c89e
Sorli Pierre <bert.dud2011@gmail.com>
2022-03-02 12:46:05 +0100

    sucessfully deploy the frontend now trying the backend then link them online not locally

--- CatWebAppBack/src/main/java/com/atelier/CatWebApp/CatWebAppApplication.java
+++ CatWebAppBack/src/main/java/com/atelier/CatWebApp/CatWebAppApplication.java
@@ -1,4 +1,4 @@
-package com.atelier.CatWebApp;
+package com.atelier.CatWebApp; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
